d5607acb284e68a3d825a9e33290b55048605d45
Varun Mhatre <varunjmhatre@gmail.com>
2019-01-28 13:58:40 -0500

    Added manager for Raycast when clicked

---
+++ Assets/Scripts/Managers/RaycastManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastManager : MonoBehaviour
+{
+
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit = new RaycastHit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            Physics.Raycast(ray, out hit, Mathf.Infinity);
+        }         
+    }
+
+    public string getRaycastHitTag()
+    {
+        Debug.Log(hit.transform.tag);
+        return hit.transform.tag;
+    }
+}
