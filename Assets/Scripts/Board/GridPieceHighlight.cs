d9ee89eb1a1213f6613487616a92aa91cea17b03
Varun Mhatre <varunjmhatre@gmail.com>
2019-02-04 23:11:14 -0500

    Added a material and a script for Highlight Grid Piece

---
+++ Assets/Scripts/Board/GridPieceHighlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPieceHighlight : MonoBehaviour
+{
+
+    [SerializeField] Material moveHighlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material = moveHighlight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
