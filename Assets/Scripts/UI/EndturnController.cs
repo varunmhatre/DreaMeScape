9c7dc90526b237fae247bef5fad21c77f4cf8953
ns7767 <ns7767@IGM2550-17>
2019-02-14 11:49:54 -0500

    Added Endturn button.

---
+++ Assets/Scripts/UI/EndturnController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EndturnController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public static bool isInteractable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInteractable = false;
+    }
+    void Update()
+    {
+        
+    } 
+    public void OnClick()
+    {
+        isInteractable = true;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.GetComponent<Image>().enabled = true;     
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(!isInteractable)
+        {
+            transform.GetComponent<Image>().enabled = false;
+        }                 
+    }
+}
