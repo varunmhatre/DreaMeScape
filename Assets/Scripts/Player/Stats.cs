8a8f52a3b0400bc93e959140bb6a6ae08143e07c
Burhanuddin Beawerwala <burhanuddin.mb@gmail.com>
2019-02-14 11:05:44 -0500

    Feature/cannons (#11)
    * Iterate on them. Get the newASSET
    
    * Important for now.
    
    * Cannon placement
    
    * Scene changes.
    
    * cannon scripts done. Waiting for playerControls for final integration.
    
    * Added Cannon animation modifiers.
    
    * Cannon cursor image added.
    
    * Added manager for Raycast when clicked
    
    * Can test Raycast in new class PlayerControls
    
    * Updated raycast manager
    
    * Data removed.
    
    * Added a material and a script for Highlight Grid Piece
    
    * When character is clicked, move spaces are highlighted
    
    * remove highlight added
    
    it's more difficult than you think lol
    
    * some tweaks for optimisation
    
    * Added Movement
    
    * Enemies added.
    
    * Pirate to work once I talk it out with varun.
    
    * Scene changes.
    
    * Cannon completed. Need to add enemies AI.
    
    * Merged with master
    
    * Forgot about the main scene changes.
    
    * Stats added
    
    
    Former-commit-id: 30243a829e7428b983260c6431e071566f0bcc34

---
+++ Assets/Scripts/Player/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField] int health;
+    [SerializeField] int damage;
+    [SerializeField] int presence;
+
+    public bool isEncumbered;
+    public bool isEnemy;
+}
