51d3366d0ed04f29efa2493bd421d8b12a54ca47
ns7767 <ns7767@IGM2550-17>
2019-02-17 22:05:18 -0500

    Generator functioning. It works.

--- Assets/Prefabs/Generators/Generator.cs
+++ Assets/Prefabs/Generators/Generator.cs
@@ -1,96 +1,70 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-//public class Generator : MonoBehaviour
-//{
-//    bool isOn = false;
-//    public Piece generator;
-//    [SerializeField] private GameObject[] onImages;
-//    [SerializeField] private GameObject offImage;
-//    [SerializeField] private GameObject uiImage;
-//    [SerializeField] private GameObject textHolder;
-//    private int numCharsToSurround;
-//    Text generatorPopupText;
-//    // Use this for initialization
-//    void Start()
-//    {
-//        generator = GetComponent<Piece>();
-//        numCharsToSurround = 3;
-        
-//        //generatorPopupText = textHolder.GetComponent<TextMesh>;
-//        //gameObject.GetComponent<TextMesh>().text = null;
+public class Generator : MonoBehaviour
+{
+    bool isOn;
+    [SerializeField] TextMesh generatorPopupText;
+    private int numCharsToSurround;
+    private int energyProvides;
+    private bool provisionAdded;
 
-//    }
+    // Use this for initialization
+    void Start()
+    {
+        provisionAdded = false;
+        isOn = false;
+        energyProvides = 2;
+        numCharsToSurround = 3;
+    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (!isOn)
-//        {
-//            isOn = CheckForPlayersAround();
-//        }
-//    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOn)
+        {
+            CheckToTurnOn();
+            UpdateTextValue();
+        }
 
-//    bool CheckForPlayersAround()
-//    {
-//        int playersAround = 0;
-//        foreach (var playerObjects in Board.possibleMoveableChars)
-//        {
-//            if (playerObjects.rowPosition <= generator.rowPosition + 1 &&
-//                playerObjects.rowPosition >= generator.rowPosition - 1 &&
-//                playerObjects.colPosition <= generator.colPosition + 1 &&
-//                playerObjects.colPosition >= generator.colPosition - 1)
-//            {
-//                playersAround++;
+        if (isOn && !provisionAdded)
+        {
+            ProvideEnergy(energyProvides);
+            provisionAdded = true;
+        }
+    }
 
-//            }
-//        }
+    private void CheckToTurnOn()
+    {
+        if (AdjacencyHandler.NumPlayerCharactersAround(gameObject, 1) >= numCharsToSurround)
+        {
+            isOn = true;
+        }
+    }
 
-//        //generatorPopupText.text = "Temp:" + displayNumber;
-//        //gameObject.GetComponent<TextMesh>().text = displayNumber.ToString();
-//        // var displayNumber = 3 - playersAround;
+    private void ProvideEnergy(int amt)
+    {
+        GameManager.totalEnergy += amt;
+    }
 
-//        if (playersAround >= 0 && playersAround < numCharsToSurround)
-//        {
-//            //float timeIncrement = Time.deltaTime * 1.6f;
-            
+    private void ChangeText(int value)
+    {
+        generatorPopupText.text = value.ToString();
+        if (value == -1)
+        {
+            generatorPopupText.text = "!!!";
+        }
+    }
 
-           
-//            uiImage.SetActive(true);
-//           /* uiImage.transform.localScale += new Vector3(timeIncrement, timeIncrement, timeIncrement);
-//            if (uiImage.transform.localScale.x >= 2.0f)
-//            {
-//                uiImage.transform.localScale += new Vector3(timeIncrement, timeIncrement, timeIncrement);
-//            }*/
-//            if (uiImage.transform.GetChild(0) != null && uiImage.transform.GetChild(0).GetComponent<TextMesh>() != null)
-//            {
-//                uiImage.transform.GetChild(0).GetComponent<TextMesh>().text = (numCharsToSurround - playersAround).ToString();
-//                uiImage.transform.GetChild(0).GetComponent<MeshRenderer>().sortingOrder = uiImage.transform.GetComponent<SpriteRenderer>().sortingOrder;
-//            }
-//        }
-//        else
-//        {
-//            uiImage.SetActive(false);
-//        }
-
-
-
-//        if (playersAround >= numCharsToSurround)
-//        {
-//            ExperimentalResources.generatorsActive++;
-//            for (int i = 0; i < onImages.Length; i++)
-//            {
-//                onImages[i].SetActive(true);
-//                if (onImages[i].GetComponent<SpriteRenderer>() != null)
-//                {
-//                    onImages[i].GetComponent<SpriteRenderer>().enabled = true;
-//                }
-//            }
-//            offImage.SetActive(false);
-//            return true;
-//        }
-//        return false;
-//    }
-//}
+    private void UpdateTextValue()
+    {
+        int result = numCharsToSurround - AdjacencyHandler.NumPlayerCharactersAround(gameObject, 1);
+        if (result <= 0)
+        {
+            result = -1;
+        }
+        ChangeText(result);
+    }
+}
