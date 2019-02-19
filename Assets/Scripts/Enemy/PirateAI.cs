f59319a3ee468a8d62f9e0e0ccaa8ca4b9743084
Burhanuddin Beawerwala <bxb5733@rit.edu>
2019-02-19 01:32:09 -0500

    Working on pirates

--- Assets/Scripts/Enemy/PirateAI.cs
+++ Assets/Scripts/Enemy/PirateAI.cs
@@ -1,21 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Entities;
 
-public class PirateAI : ComponentSystem
-{    
-    private struct pirates
+public class PirateAI : MonoBehaviour
+{
+    [SerializeField] GameObject gameManagerHandler;
+    GameManager gameManager;
+    GameObject[] pirates;
+
+    List<UnitCoordinates> pirateTurns;
+
+    bool piratesInProgress;
+    bool phase1;
+
+    float timer;
+    const float timeToWaitForEachMove = 1.0f;
+
+    int selectedPirate;
+
+    private void Start()
     {
-        public PirateCrew pieceData;
-        public Transform transform;
+        pirateTurns = new List<UnitCoordinates>();
+        piratesInProgress = false;
+        gameManager = gameManagerHandler.GetComponent<GameManager>();
+        selectedPirate = 0;
+        phase1 = false;
     }
 
-    protected override void OnUpdate()
+    public void GetAllPirates()
     {
-        foreach (var pirate in GetEntities<pirates>())
+        pirates = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    private void Update()
+    {
+        if (!gameManager.isPlayerTurn)
         {
-            
+            if (!phase1)
+            {
+                timer = 0.0f;
+                selectedPirate = 0;
+                GetAllPirates();
+            }
+            else if (piratesInProgress)
+            {
+                PirateAttack();
+            }
+            else
+            {
+                //GetPiratesCoordinates
+                GetPirateAttackMoves();
+            }
         }
-    }    
+    }
+
+    void PirateAttack()
+    {
+        
+    }
+
+    void ProgressToPlayerTurn()
+    {
+        pirateTurns.Clear();
+        timer = 0.0f;
+        phase1 = false;
+        piratesInProgress = false;
+        gameManager.isPlayerTurn = true;
+    }
+
+    void GetPirateAttackMoves()
+    {
+        if (selectedPirate == pirates.Length)
+        {
+            ProgressToPlayerTurn();
+            return;
+        }
+        UnitCoordinates closestPlayer = FindClosestPlayer();
+        //Astar to player
+        PopulateTheDestination();
+    }
+
+    void PopulateTheDestination()
+    {
+
+    }
+
+    UnitCoordinates FindClosestPlayer()
+    {
+        int shortestDistance = 10000;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        UnitCoordinates piratePosition = pirates[selectedPirate].GetComponent<UnitCoordinates>();
+        UnitCoordinates playerPosition = null;
+        for (int i = 0; i < players.Length; i++)
+        {
+            playerPosition = players[i].GetComponent<UnitCoordinates>();
+            int distance = Mathf.Abs(playerPosition.x - piratePosition.x) + Mathf.Abs(playerPosition.y - piratePosition.y);
+
+            if (distance < shortestDistance)
+            {
+                shortestDistance = distance;
+            }
+        }
+        return playerPosition;
+    }
 }
