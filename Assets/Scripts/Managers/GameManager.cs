45962885a45495c00845550f6c71c54a5f194c38
burhanuddinmb <burhanuddin.mb@gmail.com>
2019-02-20 18:22:51 -0500

    Build readying

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -37,6 +37,7 @@ public class GameManager : MonoBehaviour
         isPlayerTurn = true;
         RefreshCurrentEnergy();
         RefreshCharacters();
+        EndturnController.isInteractable = true;
     }
 
     // Update is called once per frame
