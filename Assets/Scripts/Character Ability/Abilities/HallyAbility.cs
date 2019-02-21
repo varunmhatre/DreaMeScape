24b0e453c035f6a5d08ce2336324e8f24fcd8130
Burhanuddin Beawerwala <bxb5733@rit.edu>
2019-02-21 00:00:39 -0500

    HallyAbility initiation

--- Assets/Scripts/Character Ability/Abilities/HallyAbility.cs
+++ Assets/Scripts/Character Ability/Abilities/HallyAbility.cs
@@ -13,6 +13,46 @@ public class HallyAbility : MonoBehaviour
         unitCoordinate = GetComponent<UnitCoordinates>();
     }
 
+    void AddPig()
+    {
+        foreach (var item in GridMatrix.gameGrid)
+        {
+            GridPiece gridPiece = item.transform.GetComponent<GridPiece>();
+            if (gridPiece.unit != null)
+            {
+                continue;
+            }
+            int x = item.x;
+            int y = item.y;
+            if (x > 7 && x < 12 && y > 2 && y < 6)
+            {
+                pigPrefab.GetComponent<UnitCoordinates>().SetUnitCoordinates(x, y);
+                gridPiece.unit = Instantiate(pigPrefab, item.transform.position, Quaternion.identity);
+                CharacterManager.allHallyCharacters.Add(gridPiece.unit);
+                break;
+            }
+        }
+    }
 
-
+    void AddBird()
+    {
+        foreach (var item in GridMatrix.gameGrid)
+        {
+            GridPiece gridPiece = item.transform.GetComponent<GridPiece>();
+            if (gridPiece.unit != null)
+            {
+                continue;
+            }
+            int x = item.x;
+            int y = item.y;
+            if (x > 7 && x < 12 && y > 2 && y < 6)
+            {
+                birdPrefab.GetComponent<UnitCoordinates>().SetUnitCoordinates(x, y);
+                gridPiece.unit = Instantiate(birdPrefab, item.transform.position, Quaternion.identity);
+                CharacterManager.allHallyCharacters.Add(gridPiece.unit);
+                CharacterManager.allCharacters.Add(gridPiece.unit);
+                break;
+            }
+        }
+    }
 }
