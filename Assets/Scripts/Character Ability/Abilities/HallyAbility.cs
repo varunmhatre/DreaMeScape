66522eec19de904e23794e42c320e7f4ea42f365
Burhanuddin Beawerwala <burhanuddin.mb@gmail.com>
2019-02-20 23:29:51 -0500

    Two pIrates added after threeturns.
    Former-commit-id: fbff70b8b557d49a93d152634470738d1beacecf

---
+++ Assets/Scripts/Character Ability/Abilities/HallyAbility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallyAbility : MonoBehaviour
+{
+    [SerializeField] GameObject pigPrefab;
+    [SerializeField] GameObject birdPrefab;
+    UnitCoordinates unitCoordinate;
+
+    private void Start()
+    {
+        unitCoordinate = GetComponent<UnitCoordinates>();
+    }
+
+
+
+}
