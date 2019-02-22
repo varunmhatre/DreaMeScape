d5fec6fc4ca6de44de3b5e342010689d5f0f6fc5
Naveen Sampath <ns7767@ad.rit.edu>
2019-02-22 09:42:04 -0500

    Added audio and setup for settings/controls.
    Former-commit-id: 59ee7f337d50e73b1a2afb2e4dcb5a9a630818a2

--- Assets/Scripts/UI/VolumeControls.cs
+++ Assets/Scripts/UI/VolumeControls.cs
@@ -1,26 +1,24 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 
 public class VolumeControls : MonoBehaviour
 {
-    private int volumeControl;
-    public Slider volumeSlider;
-    // Start is called before the first frame update
+    private AudioSource audioSrc;
+    private float musicVolume = 1f;
+
     void Start()
     {
-        
+        audioSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        audioSrc.volume = musicVolume;
+    }
+    public void SetVolume(float vol)
     {
-       
-    } 
-
-    public void ChangeVolume()
-    {
-        transform.GetComponent<Slider>().value = volumeControl;
+        musicVolume = vol;
     }
 }
