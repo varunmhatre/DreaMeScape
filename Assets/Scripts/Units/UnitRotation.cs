23e508abfcdee83efa0641a4047f79b5876b9270
Burhanuddin Beawerwala <bxb5733@rit.edu>
2019-03-01 12:25:47 -0500

    Generator rotation fix.

--- Assets/Scripts/Units/UnitRotation.cs
+++ Assets/Scripts/Units/UnitRotation.cs
@@ -4,23 +4,9 @@ using UnityEngine;
 
 public class UnitRotation : MonoBehaviour
 {
-    Quaternion rotation;
-
     // Start is called before the first frame update
     void Start()
     {
-        rotation = transform.rotation;   
     }
 
-    public void SetRotation (Quaternion newRotation)
-    {
-        rotation = newRotation;
-        transform.rotation = newRotation;
-    }
-
-    public void SetLocalRotation(Quaternion newRotation)
-    {
-        rotation = newRotation;
-        transform.localRotation = newRotation;
-    }
 }
