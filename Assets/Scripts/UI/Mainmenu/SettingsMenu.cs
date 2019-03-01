02b37605ea798b1602f6314c7734254aef6c8964
Naveen Sampath <ns7767@ad.rit.edu>
2019-03-01 13:48:25 -0500

    Added all control settings in the main menu.
    Former-commit-id: 7abf21115562e1db3b7fad9b16464048a8bc677a

---
+++ Assets/Scripts/UI/Mainmenu/SettingsMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    int currentResolutionIndex; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if ((resolutions[i].width == Screen.currentResolution.width) && (resolutions[i].height == Screen.currentResolution.height)) ;
+            {
+                currentResolutionIndex = i;
+            }
+         }
+
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        resolutionDropdown.AddOptions(options);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetQuality(int index) 
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
