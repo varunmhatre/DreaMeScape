2072e2e2b95aa03947960034c10fa0dd2e98ece6
ns7767 <ns7767@IGM2550-17>
2019-03-04 13:54:34 -0500

    Initial setup for character selection on HUD.

---
+++ Assets/Scripts/UI/HUD/CharacterSelection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    //[SerializeField] private Button[] characters;
+    [SerializeField] private int clickID;
+   // [SerializeField] private GameObject playerControls;
+
+    private string selectedUnitName; 
+    private bool piecesHighlighted;
+    private int[] playerLoc;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CharacterSelect()
+    {
+        Debug.Log("ClickID:      " + clickID);
+        
+
+       // gameObject.GetComponent<GridPieceSelect>().highlightMoveSpaces(playerName: selectedUnitName, toHighlight: true, playerLocation: playerLoc);
+       // piecesHighlighted = true;
+    } 
+}
