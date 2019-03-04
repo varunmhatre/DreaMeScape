eecdc31a58224e8226b5233215b8af0baafa4663
ns7767 <ns7767@IGM2550-17>
2019-03-04 17:23:38 -0500

    HUD character selection is implemented. But needs to be tested.
    Former-commit-id: af261fc665fc81e7d2a5ba8fcae3decd8b2523cd

---
+++ Assets/Scripts/UI/HUD/testScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class testScript : MonoBehaviour
+{ 
+    public EventSystem eventSystem; //select event system in editor
+    private GameObject lastSelectedButton;
+    private GameObject currentSelectedButton;
+    private GameObject currentSelectedGameObject_Recent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+     
+    public void CharacterSelect()
+    {
+        GetLastGameObjectSelected();
+    }
+    private void GetLastGameObjectSelected()
+    {
+        if (eventSystem.currentSelectedGameObject != currentSelectedGameObject_Recent)
+        {
+            lastSelectedButton = currentSelectedGameObject_Recent;
+            currentSelectedGameObject_Recent = eventSystem.currentSelectedGameObject;
+            Debug.Log("lastSelectedGameObject:  " + lastSelectedButton);
+            Debug.Log("currentSelectedGameObject_Recent:  " + currentSelectedGameObject_Recent); 
+        }
+    }
+}
