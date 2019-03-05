644e52c2b26e9545c0a95b85d65a540dbd013ef6
Naveen Sampath <ns7767@ad.rit.edu>
2019-03-05 08:19:47 -0500

    Implementation on the process of hud character selection.

--- Assets/Scripts/UI/HUD/testScript.cs
+++ Assets/Scripts/UI/HUD/testScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class testScript : MonoBehaviour
 { 
@@ -9,11 +10,15 @@ public class testScript : MonoBehaviour
     private GameObject lastSelectedButton;
     private GameObject currentSelectedButton;
     private GameObject currentSelectedGameObject_Recent;
+    //[SerializeField] private int characterID; 
 
+    [SerializeField] private GameObject Manager;
+
+    //[SerializeField] private int characterID;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -24,16 +29,30 @@ public class testScript : MonoBehaviour
      
     public void CharacterSelect()
     {
-        GetLastGameObjectSelected();
+        GetLastGameObjectSelected(); 
     }
     private void GetLastGameObjectSelected()
     {
+        int currentID = transform.GetComponent<CharacterID>().clickID;
+
+        Debug.Log("currentID:       " + currentID);
         if (eventSystem.currentSelectedGameObject != currentSelectedGameObject_Recent)
         {
-            lastSelectedButton = currentSelectedGameObject_Recent;
+            lastSelectedButton = currentSelectedGameObject_Recent;            
             currentSelectedGameObject_Recent = eventSystem.currentSelectedGameObject;
-            Debug.Log("lastSelectedGameObject:  " + lastSelectedButton);
-            Debug.Log("currentSelectedGameObject_Recent:  " + currentSelectedGameObject_Recent); 
+
+           // Debug.Log("lastSelectedGameObject:  " + lastSelectedButton);
+            Debug.Log("currentSelectedGameObject_Recent:  " + currentSelectedGameObject_Recent.name);
         }
+        //int[] playerLoc = { CharacterManager.allAlliedCharacters[characterID].GetComponent<UnitCoordinates>().x, CharacterManager.allAlliedCharacters[characterID].GetComponent<UnitCoordinates>().y };
+        //Manager.GetComponent<GridPieceSelect>().highlightMoveSpaces(playerName: currentSelectedGameObject_Recent.name, toHighlight: true, playerLocation: playerLoc);
+               
+        if (lastSelectedButton != null)
+        {
+            Debug.Log("lastSelectedGameObject:  " + lastSelectedButton.name);
+           // Manager.GetComponent<GridPieceSelect>().highlightMoveSpaces(playerName: lastSelectedButton.name, toHighlight: false, playerLocation: null);
+        }
+        
+        
     }
 }
