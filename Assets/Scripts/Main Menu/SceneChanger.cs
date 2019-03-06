fe8aeaf143f5e9d9d2783598f55a234ac9ed2427
Neevan09 <36045501+Neevan09@users.noreply.github.com>
2019-03-06 12:09:25 -0500

    Feature/main menu (#39)
    * Main Menu skeleton
    
    * Main Menu update
    
    * Main Menu Update 2
    
    * Fixed uniform opening menu buttons.
    
    * Exit button working.
    
    * Added setup for character description in settings.
    
    * Added character description in settings menu.
    
    * Added all control settings in the main menu.
    
    * Added pause game.
    
    * Initial setup for character selection on HUD.
    
    * HUD character selection is implemented. But needs to be tested.
    
    * Implementation on the process of hud character selection.
    
    * Changes made to HUD Prefab.
    
    * Highlight space working. Need to be tested.
    
    * Added exit in the settings menu.
    
    * Uncommented the test code\ in player controls script.
    
    
    Former-commit-id: 5d0e2b9fddbcd9e3172cd69f71c395c2181550aa

---
+++ Assets/Scripts/Main Menu/SceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour {
+
+	public void LoadNextScene()
+    {
+        int currentSceneInd = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneInd + 1);
+    }
+}
---
+++ Assets/Scripts/UI/HUD/SelectCharacterHUD.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectCharacterHUD : MonoBehaviour
+{
+    [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private GameObject Manager;
+
+    private GameObject lastSelectedButton;
+    private GameObject currentSelectedButton;
+
+    private int characterID;
+    private int index;
+    private int[] playerLoc;
+
+    private string[] characters;
+    private enum CharacterType
+    {
+        Ed, Hally, Kent, Jade, Meda
+    }
+
+    //[SerializeField] private int characterID; 
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        characters = new string[]{"Ed", "Hally", "Jade", "Kent", "Meda"};
+        playerLoc = new int[2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     
+    public void CharacterSelect()
+    {        
+        if (eventSystem.currentSelectedGameObject != currentSelectedButton)
+        {
+            lastSelectedButton = currentSelectedButton;
+            currentSelectedButton = eventSystem.currentSelectedGameObject; 
+        }
+        for (index = 0; index < CharacterManager.allAlliedCharacters.Count; index++)
+        {
+            //Debug.Log("characters[index]:     " + characters[index]);
+            //Debug.Log("currentSelectedButton.name:     " + currentSelectedButton.name);
+            if (characters[index] == currentSelectedButton.name)
+            {
+                characterID = index; 
+            }
+        }   
+
+        Debug.Log("characterID:     " + characterID);   
+
+        //Mark: Disable the highlight space.
+        if (lastSelectedButton != null)
+        { 
+            Manager.GetComponent<GridPieceSelect>().highlightMoveSpaces(playerName: lastSelectedButton.name, toHighlight: false, playerLocation: null);
+            Debug.Log("lastSelectedButton.name:     " + lastSelectedButton.name);
+        }
+
+        //Mark: Enable the highlight space
+        if (currentSelectedButton != null)
+        {
+            playerLoc[0] = CharacterManager.allAlliedCharacters[characterID].GetComponent<UnitCoordinates>().x;
+            playerLoc[1] = CharacterManager.allAlliedCharacters[characterID].GetComponent<UnitCoordinates>().y;
+            Manager.GetComponent<GridPieceSelect>().highlightMoveSpaces(playerName: characters[characterID], toHighlight: true, playerLocation: playerLoc);
+            //Manager.GetComponent<PlayerControls>().SetSelectedUnit(currentSelectedButton.transform);
+        }
+
+
+    }
+}
