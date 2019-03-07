c9826e2372154488caa2a69d47983054341d8cd8
ns7767 <ns7767@IGM2550-17>
2019-03-07 15:19:58 -0500

    Stuff added to select character pn HUD.
    Former-commit-id: 1def0be5570738df94514a554af5a73a6c86cb9f [formerly 7ebf3889c60d916978f6cea30d37afb211109602]
    Former-commit-id: ec22effb2c010c880c22ba713925db0d0201dfa7

--- Assets/Scripts/UI/HUD/DreamMeter.cs
+++ Assets/Scripts/UI/HUD/DreamMeter.cs
@@ -6,6 +6,7 @@ public class DreamMeter : MonoBehaviour
 {
     [SerializeField] RectTransform[] dreamMeterBarArr;
     private float startingWidth;
+    private float startingHeight;
     private int meterValue;
 
     // Start is called before the first frame update
@@ -13,12 +14,14 @@ public class DreamMeter : MonoBehaviour
     {
         if (dreamMeterBarArr[0] != null)
         {
-            startingWidth = dreamMeterBarArr[0].rect.width;
+            //startingWidth = dreamMeterBarArr[0].rect.width;
+            startingHeight = dreamMeterBarArr[0].rect.height;
         }
 
         for (int i = 0; i < dreamMeterBarArr.Length; i++)
         {
-            dreamMeterBarArr[i].sizeDelta = new Vector2(0.0f, dreamMeterBarArr[i].rect.height);
+            //dreamMeterBarArr[i].sizeDelta = new Vector2(0.0f, dreamMeterBarArr[i].rect.height);
+            dreamMeterBarArr[i].sizeDelta = new Vector2(dreamMeterBarArr[i].rect.width, 0.0f);
         }
     }
     // Update is called once per frame
@@ -32,8 +35,13 @@ public class DreamMeter : MonoBehaviour
         {
             meterValue = CharacterManager.allAlliedCharacters[i].GetComponent<Stats>().meterUnitsFilled;
 
-            dreamMeterBarArr[i].sizeDelta = new Vector2(startingWidth * meterValue / CharacterManager.allAlliedCharacters[i].GetComponent<Stats>().maxMeter, dreamMeterBarArr[i].rect.height);
-            dreamMeterBarArr[i].GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 2, dreamMeterBarArr[i].rect.width);
+            //This is for Horizontal fill meter.
+           // dreamMeterBarArr[i].sizeDelta = new Vector2(startingWidth * meterValue / CharacterManager.allAlliedCharacters[i].GetComponent<Stats>().maxMeter, dreamMeterBarArr[i].rect.height);
+           // dreamMeterBarArr[i].GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 2, dreamMeterBarArr[i].rect.width);
+
+            //This is for vertical fill meter.
+            dreamMeterBarArr[i].sizeDelta = new Vector2(dreamMeterBarArr[i].rect.width,startingHeight * meterValue / CharacterManager.allAlliedCharacters[i].GetComponent<Stats>().maxMeter);
+            dreamMeterBarArr[i].GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 2, dreamMeterBarArr[i].rect.height);
         }
     }
 }
