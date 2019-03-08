e00cb926fe4255085da9c695491e69beb982a27d
ns7767 <ns7767@IGM2550-17>
2019-03-08 14:08:44 -0500

    Added arrow keys for camera controls.
    Former-commit-id: 90df54ac2cdcb812ddd4195e13dec5740747fed4 [formerly 980cd58f6f2d53a02f654478841934de85df92d2]
    Former-commit-id: 39ce1b3f65d9000a0a32ac868505bdd0ab3a8eda

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -76,11 +76,11 @@ public class CameraMovement : MonoBehaviour {
             {
                 //check to see if you are moving the camera up, down, left, or right
                 MoveCamera(Input.GetAxis("SecondaryCommandHoriz"), Input.GetAxis("SecondaryCommandVert"));
-                //MoveCamera(Input.GetAxis("CameraCommandHoriz"), Input.GetAxis("CameraCommandVert"));
+                MoveCamera(Input.GetAxis("CameraCommandHoriz"), Input.GetAxis("CameraCommandVert"));
                 ZoomCamera(Input.GetAxis("Mouse Scrollwheel"), zoomInMax, zoomOutMin);
                 SwapCameras(Input.GetKeyDown(KeyCode.L));
             }
-        }
+        } 
         else if (firstTime == true)
         {
             PirateShipMoveSetup(positions, times);
