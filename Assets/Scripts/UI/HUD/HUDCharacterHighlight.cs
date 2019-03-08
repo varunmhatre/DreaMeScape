cc97c590e0c3b4a5093f876911d492cdbea959dc
ns7767 <ns7767@IGM2550-17>
2019-03-08 15:33:53 -0500

    Fixed character highlight and updated the HUD Icons
    Former-commit-id: dde62669b1a790e355111b2215ede518024274ec [formerly 51faaf4125d24094f10be7a25de5591e2ff1c696]
    Former-commit-id: 8f742f10f0db9798329054f46e7acc633934965b

---
+++ Assets/Scripts/UI/HUD/HUDCharacterHighlight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDCharacterHighlight : MonoBehaviour
+{
+    [SerializeField] private GameObject[] character;
+
+    private static string[] characters;
+    private static int[] characterID;
+    private static int index;
+    private static int clickID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characters = new string[] { "Ed", "Hally", "Jade", "Kent", "Meda" };
+        characterID = new int[5];
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            characterID[i] = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void HighlightPortrait()
+    {
+        Debug.Log("ClickID:    " + clickID);
+        for (index = 0; index < CharacterManager.allAlliedCharacters.Count; index++)
+        {
+            if (characters[index] == CharacterManager.allAlliedCharacters[index].name)
+            {
+                clickID = index;
+                
+            }
+        }
+    }
+}
