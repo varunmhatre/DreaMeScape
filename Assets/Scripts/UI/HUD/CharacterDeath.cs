b449c3651a024fae50ca6e55516f8f07ce5c1bcc
ns7767 <ns7767@IGM2550-17>
2019-03-12 16:00:42 -0400

    HUD Polished. Stuff added to the death panel enable on the portrait.

--- Assets/Scripts/UI/HUD/CharacterDeath.cs
+++ Assets/Scripts/UI/HUD/CharacterDeath.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class CharacterDeath : MonoBehaviour
 {
+    private int healthValue;
+    private int deathValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        deathValue = 0;
     }
-
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < CharacterManager.allAlliedCharacters.Count; i++)
-        {
-            if (CharacterManager.allAlliedCharacters[i].GetComponent<Stats>().health  <= 0)
-            {
-
-            }
-        }
+        EnableDeathPanel();
     }
 
-
-    void Character()
+    void EnableDeathPanel()
     {
-
+        for (int i = 0; i < CharacterManager.allAlliedCharacters.Count; i++)
+        {
+            healthValue = CharacterManager.allAlliedCharacters[i].GetComponent<Stats>().health;  
+                
+            if (healthValue <= deathValue)
+            {
+                Debug.Log("Enable the death panel");
+            }
+        }
     }
 }
