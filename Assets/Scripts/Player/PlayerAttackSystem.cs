0ba8b03dc6f74c4e12f14e98db0c008f28d90a70
Burhanuddin Beawerwala <bxb5733@rit.edu>
2019-04-26 19:24:27 -0400

    playerbases + tutorial fix

---
+++ Assets/Scripts/Player/PlayerAttackSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class PlayerAttackSystem : ComponentSystem
+{
+    public struct AttackComponents
+    {
+        public Stats stats;
+        public PlayerBase playerBase;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach (var item in GetEntities<AttackComponents>())
+        {
+            if (item.stats.hasAttacked)
+            {
+                item.playerBase.baseSprite.sprite = item.playerBase.offVisual;
+            }
+            else
+            {
+                item.playerBase.baseSprite.sprite = item.playerBase.onVisual;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    [SerializeField] public SpriteRenderer baseSprite;
+
+    [SerializeField] public Sprite onVisual;
+    [SerializeField] public Sprite offVisual;
+}
