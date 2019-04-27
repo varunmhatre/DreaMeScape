3a0727b856b6bc22737a321ece7ea1a840ec96db
Burhanuddin Beawerwala <burhanuddin.mb@gmail.com>
2019-04-26 21:33:17 -0400

    Attack UI changes + bug fixes (#68)
    * Added splash screen.
    
    * Added image logo animation in the splah screen.
    
    * Changes to logo animation.
    
    * Updated the dialogue manager for the henry and pirate.
    
    * Fixed character prefab heath and attack icon.
    
    * Added logo animation
    
    * Logo animation updated.
    
    * Splash screen is done.
    
    * Fixed generator. NOTE - Missing from the level manager.
    
    * Fixed kent dialogue portrait.
    
    * Fixed font color, updated the HUD.
    
    * Character page has updated.
    
    * Update the stats UI of characters.
    
    * Fixed missing textrures.
    
    * Character page is done.
    
    * Fonts updated.
    
    * Fixed mouse hover cursor. Need testing.
    
    * Updated the heart asset in HUD.
    
    * Updated the attach asset.
    
    * Fixed mouse hover, when character selected, attack cursor shows up.
    
    * Energy animation is setup done.
    
    * Energy end animation is done.
    
    * Update the pirate dialogue portrait.
    
    * Added energy and special ability animations.
    
    * Energy animation.
    
    * Added attack on the HUD.
    
    * playerbases + tutorial fix
    
    * In progress
    
    * Attached attack changes
    
    * Much needed cursor change.
    
    * Energy used and end animation are fixed.
    
    * Changes for attack
    
    * Dialogbox fix

---
+++ Assets/Scripts/Player/PlayerAttackSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class PlayerAttackSystem : ComponentSystem
+{
+    public struct AttackComponents
+    {
+        public Stats stats;
+        public PlayerBase playerBase;
+        public PlayerAttackUI attackUI;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach (var item in GetEntities<AttackComponents>())
+        {
+            if (item.stats.hasAttacked)
+            {
+                item.playerBase.baseSprite.sprite = item.playerBase.offVisual;
+                item.attackUI.baseVisual.sprite = item.attackUI.offVisual;
+            }
+            else
+            {
+                item.playerBase.baseSprite.sprite = item.playerBase.onVisual;
+                item.attackUI.baseVisual.sprite = item.attackUI.offVisual;
+            }
+        }
+    }
+}
