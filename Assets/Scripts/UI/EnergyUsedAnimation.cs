8cb7515d9f0222fb649e05a820f2388e0426316e
Naveen Sampath <ns7767@ad.rit.edu>
2019-04-26 20:46:00 -0400

    Energy used and end animation are fixed.

---
+++ Assets/Scripts/UI/EnergyUsedAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyUsedAnimation : MonoBehaviour
+{
+    public static bool enableAnim;
+    // Use this for initialization
+    void Start()
+    {
+        enableAnim = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enableAnim)
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().enabled = false;
+        }
+    }
+}
